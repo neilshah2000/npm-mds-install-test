3facd385063510b6b1499a10aecff6a14f928009
neilshah2000 <neilshahlimited@hotmail.com>
2022-10-26 11:04:13 +0100

    updated to include direct mds calls

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import { useEffect, useState } from "react";
-import { commands, events } from "npm-upload-9781";
+import { commands, events, Coin, MDS, Decimal } from "npm-upload-9781";
 
 function App() {
   const [minimaBalance, setMinimaBalance] = useState("");
 
   useEffect(async () => {
+    // Decimal.js is used to handle floating point numbers
+    Decimal.set({ precision: 60 });
+
     // initialize minima before calling any commands
     events.onInit(async () => {
       const balance = await commands.balance();
       const balanceString = JSON.stringify(balance, null, 2);
       setMinimaBalance(balanceString);
+    });
+
+    // dont't call MDS directly because we have commands, events and sql helpers,
+    // but it's still there if you need an escape hatch
+    MDS.init(() => {
+      MDS.cmd("balance", (res) => {
+        console.log("minima cmd called directly", res);
+      });
     });
   }, []);
 
